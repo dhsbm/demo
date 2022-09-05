1802e7b75c565096f4b909d604a9682954bd46d7
gosted <9172940+du-hao-111@user.noreply.gitee.com>
2022-09-05 18:21:50 +0800

    完善ajax函数封装及更新README文档

--- 封装ajax/index.js
+++ 封装ajax/index.js
@@ -1,6 +1,5 @@
 const noop = () => {}
 let defaults = {
-  url: '', // 请求地址
   type: 'GET', // 请求类型
   async: true, // 是否异步执行
   headers: {}, // 请求头信息
@@ -17,22 +16,23 @@ let defaults = {
 }
 
 // 封装xhr请求，参数说明见defaults对象
-function ajax(o) {
-  const options = Object.assign({}, defaults, o)
+function ajax(url, options) {
+  const config = Object.assign({}, defaults, options)
   const xhr = new XMLHttpRequest()
-  xhr.open(options.type, options.url, options.async)
+  xhr.open(config.type, url, config.async)
+  xhr.timeout = config.timeout
   // 设置请求头
-  for (const key in options.headers) {
-    xhr.setRequestHeader(key, options.headers[key])
+  for (const key in config.headers) {
+    xhr.setRequestHeader(key, config.headers[key])
   }
   // 注册事件
   const events = ['readystatechange', 'abort', 'error', 'load', 'loadstart', 'loadend', 'progress', 'overtime']
   for (const event of events) {
     xhr.addEventListener(event, () => {
-      options[event](xhr)
+      config[event](xhr)
     })
   }
-  xhr.send(options.data)
+  xhr.send(config.data)
 
   // 返回一个终止请求的函数
   return function () {
@@ -43,22 +43,22 @@ function ajax(o) {
 // 默认的检测成功请求的函数
 const checkSuccess = (res) => (res.status >= 200 && res.status < 300) || res.status === 304
 // 自动重发
-function autoResend(o, count = 0, isSuccess = checkSuccess) {
+function autoResend(url, options = {}, count = 0, isSuccess = checkSuccess) {
   // 通过结束事件检测是否成功，默认依照状态码判断
-  const preLoadend = o.loadend
-  o.loadend = (xhr) => {
+  const preLoadend = options.loadend
+  options.loadend = (xhr) => {
     if (preLoadend) {
       preLoadend(xhr)
     }
     // 错误重发
     if (!isSuccess(xhr) && count > 0) {
-      abort = autoResend(o, count - 1, isSuccess)
+      abort = autoResend(url, options, count - 1, isSuccess)
       count = 0
     }
   }
 
   let abort
-  abort = ajax(o)
+  abort = ajax(url, options)
   // 返回一个终止请求的函数
   return () => {
     count = 0 // 数字置0，避免重发
@@ -68,29 +68,28 @@ function autoResend(o, count = 0, isSuccess = checkSuccess) {
 
 // 使用promise时，一般不会绑定事件函数
 // 简单的promise封装，一定会返回解决的promise
-function ajaxWithPromise(o) {
+function ajaxWithPromise(url, options = {}) {
   return new Promise((resolve, reject) => {
-    o.loadend = (xhr) => {
+    options.loadend = (xhr) => {
       resolve(xhr)
     }
-    ajax(o)
+    ajax(url, options)
   })
 }
-// 封装fetch重发功能
-function autoResendFetch(o, count = 0, isSuccess = checkSuccess) {
-  const options = Object.assign({}, defaults, o)
 
-  return fetch(options.url, options).then(
+// 封装fetch重发功能
+function autoResendFetch(url, config, count = 0, isSuccess = checkSuccess) {
+  return fetch(url, config).then(
     (response) => {
       if (count > 0 && !isSuccess(response)) {
-        return autoResendFetch(o, count - 1, isSuccess)
+        return autoResendFetch(url, config, count - 1, isSuccess)
       } else {
         return response
       }
     },
     (err) => {
       if (count > 0) {
-        return autoResendFetch(o, count - 1, isSuccess)
+        return autoResendFetch(url, config, count - 1, isSuccess)
       } else {
         return Promise.reject(err)
       }
@@ -102,8 +101,7 @@ function autoResendFetch(o, count = 0, isSuccess = checkSuccess) {
 
 // 测试ajax事件功能
 const test1 = (path, port = '3000') => {
-  return ajax({
-    url: 'http://localhost:' + port + path,
+  return ajax('http://localhost:' + port + path, {
     load(xhr) {
       console.log(path + '请求完成，状态码为：' + xhr.status)
     },
@@ -115,18 +113,16 @@ const test1 = (path, port = '3000') => {
     },
   })
 }
-// 测试正常请求 返回200
-false && test1('/success')
-// 测试失败请求 返回400 不触发error
-false && test1('/fail')
-// 测试跨域请求 状态码为0 触发error
-false && test1('/cross', '3001')
+
+false && test1('/success') // 测试正常请求 状态码为200
+false && test1('/fail') // 测试失败请求 状态码为400 不触发error
+false && test1('/cross', '3001') // 测试跨域请求 状态码为0 触发error
 
 // 测试请求重发功能
 const test2 = (path, port = '3000', count = 3) => {
   return autoResend(
+    'http://localhost:' + port + path,
     {
-      url: 'http://localhost:' + port + path,
       load(xhr) {
         console.log(path + '请求完成，状态码为：' + xhr.status)
       },
@@ -140,41 +136,34 @@ const test2 = (path, port = '3000', count = 3) => {
     count
   )
 }
-// 测试成功请求的重发
-false && test2('/success')
-// 测试失败请求的重发
-false && test2('/fail')
-// 测试跨域请求的重发
-false && test2('/cross', '3001')
+
+false && test2('/success') // 测试成功请求 无重发
+false && test2('/fail') // 测试失败请求 3次重发 时间稳定
+false && test2('/cross', '3001') // 测试跨域请求 3次重发 时间不稳定
 
 // 测试中断功能
 const abort1 = () => setTimeout(test1('/success'), 1500)
-const abort2 = () => setTimeout(test2('/fail'), 1500)
+const abort2 = () => setTimeout(test2('/fail'), 2500)
 const abort3 = () => setTimeout(test2('/cross', '3001'), 3000)
-false && abort1()
-false && abort2()
-false && abort3()
+false && abort1() // 测试成功请求 仅触发loadend 状态码为0
+false && abort2() // 测试失败请求 请求仅重发2次，且第2次重发被打断
+false && abort3() // 测试跨域请求 请求仅重发2次，且第2次重发被打断
 
 // 测试promise
 const test3 = (path, port = '3000') => {
-  return ajaxWithPromise({ url: 'http://localhost:' + port + path })
+  return ajaxWithPromise('http://localhost:' + port + path)
     .then((xhr) => {
       console.log(xhr)
     })
     .catch((err) => console.log(err))
 }
-false && test3('/success')
-false && test3('/fail')
-false && test3('/cross', '3001')
+false && test3('/success') // 测试正常请求 状态码为200
+false && test3('/fail') // 测试失败请求 状态码为400
+false && test3('/cross', '3001') // 测试跨域请求 状态码为0
 
 // 测试重发fetch
 const test4 = (path, port = '3000', count = 3) => {
-  autoResendFetch(
-    {
-      url: 'http://localhost:' + port + path,
-    },
-    3
-  )
+  autoResendFetch('http://localhost:' + port + path, {}, count)
     .then((response) => {
       console.log(response)
       return response.text()
@@ -186,6 +175,6 @@ const test4 = (path, port = '3000', count = 3) => {
       console.log(err)
     })
 }
-false && test4('/success')
-false && test4('/fail')
-false && test4('/cross', '3001')
+true && test4('/success') // 测试成功fetch请求 无重发
+false && test4('/fail') // 测试失败fetch请求 3次重发
+false && test4('/cross', '3001') // 测试跨域fetch请求 3次重发 显示错误信息
--- 封装ajax/index.ts
+++ 封装ajax/index.ts
@@ -1,6 +1,5 @@
 const noop = () => {}
-const defaults = {
-  url: '', // 请求地址
+let defaults = {
   type: 'GET', // 请求类型
   async: true, // 是否异步执行
   headers: {}, // 请求头信息
@@ -9,7 +8,7 @@ const defaults = {
   readystatechange: noop, //  readyState 属性发生变化时执行事件
   abort: noop, // 请求停止时执行事件
   error: noop, // 请求失败后执行事件
-  load: noop, // 请求成功后执行事件
+  load: noop, // 请求完成后执行事件
   loadstart: noop, // 接收到响应数据时执行事件
   loadend: noop, // 请求结束时执行事件
   progress: noop, // 请求接收到更多数据时执行事件
@@ -17,47 +16,49 @@ const defaults = {
 }
 
 // 封装xhr请求
-function ajax(o: Options): () => void {
-  const options = Object.assign(defaults, o)
+function ajax(url: string, options?: Options) {
+  const config = Object.assign({}, defaults, options)
   const xhr = new XMLHttpRequest()
-  xhr.open(options.type, options.url, options.async)
+  xhr.open(config.type, url, config.async)
   // 设置请求头
-  for (const key in options.headers) {
-    xhr.setRequestHeader(key, options.headers[key])
+  for (const key in config.headers) {
+    xhr.setRequestHeader(key, config.headers[key])
   }
   // 注册事件
   const events = ['readystatechange', 'abort', 'error', 'load', 'loadstart', 'loadend', 'progress', 'overtime']
   for (const event of events) {
     xhr.addEventListener(event, () => {
-      options[event](xhr)
+      config[event](xhr)
     })
   }
-  xhr.send(options.data)
+  xhr.send(config.data)
 
   // 返回一个终止请求的函数
   return function () {
     xhr.abort()
   }
 }
+
 // 默认的检测成功请求的函数
-const checkSuccess = (xhr: XMLHttpRequest) => (xhr.status >= 200 && xhr.status < 300) || xhr.status === 304
+const checkSuccess = (res: Response | XMLHttpRequest): boolean =>
+  (res.status >= 200 && res.status < 300) || res.status === 304
 // 自动重发
-function autoResend(o: Options, count = 0, isSuccess = checkSuccess) {
-  // 通过结束事件检测是否成功
-  const preLoadend = o.loadend
-  o.loadend = (xhr: XMLHttpRequest) => {
+function autoResend(url: string, options: Options = {}, count = 0, isSuccess = checkSuccess): () => void {
+  // 通过结束事件检测是否成功，默认依照状态码判断
+  const preLoadend = options.loadend
+  options.loadend = (xhr: XMLHttpRequest) => {
     if (preLoadend) {
       preLoadend(xhr)
     }
     // 错误重发
     if (!isSuccess(xhr) && count > 0) {
-      abort = autoResend(o, count - 1, isSuccess)
+      abort = autoResend(url, options, count - 1, isSuccess)
       count = 0
     }
   }
 
-  let abort: () => void
-  abort = ajax(o)
+  let abort
+  abort = ajax(url, options)
   // 返回一个终止请求的函数
   return () => {
     count = 0 // 数字置0，避免重发
@@ -65,32 +66,49 @@ function autoResend(o: Options, count = 0, isSuccess = checkSuccess) {
   }
 }
 
-// 简单的promise封装
-function ajaxWithPromise(o: Options) {
+// 使用promise时，一般不会绑定事件函数
+// 简单的promise封装，一定会返回解决的promise
+function ajaxWithPromise(url: string, options: Options = {}) {
   return new Promise((resolve, reject) => {
-    o.loadend = (xhr: XMLHttpRequest) => {
+    options.loadend = (xhr: XMLHttpRequest) => {
       resolve(xhr)
     }
-    ajax(o)
+    ajax(url, options)
   })
 }
+// 封装fetch重发功能
+function autoResendFetch(url: string, config?: RequestInit, count = 0, isSuccess = checkSuccess) {
+  return fetch(url, config).then(
+    (response) => {
+      if (count > 0 && !isSuccess(response)) {
+        return autoResendFetch(url, config, count - 1, isSuccess)
+      } else {
+        return response
+      }
+    },
+    (err) => {
+      if (count > 0) {
+        return autoResendFetch(url, config, count - 1, isSuccess)
+      } else {
+        return Promise.reject(err)
+      }
+    }
+  )
+}
 
-function autoResendFetch(o) {}
-
-type AjaxNoop = (() => void) | ((xhr: XMLHttpRequest) => void)
+type AjaxHandler = (() => void) | ((xhr: XMLHttpRequest) => void)
 interface Options {
-  url: string
   type?: string
   async?: boolean
   headers?: { [propName: string]: string }
   timeout?: number
   data?: any
-  readystatechange?: AjaxNoop
-  abort?: AjaxNoop
-  error?: AjaxNoop
-  load?: AjaxNoop
-  loadstart?: AjaxNoop
-  loadend?: AjaxNoop
-  progress?: AjaxNoop
-  overtime?: AjaxNoop
+  readystatechange?: AjaxHandler
+  abort?: AjaxHandler
+  error?: AjaxHandler
+  load?: AjaxHandler
+  loadstart?: AjaxHandler
+  loadend?: AjaxHandler
+  progress?: AjaxHandler
+  overtime?: AjaxHandler
 }

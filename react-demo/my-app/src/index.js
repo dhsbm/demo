f134c6dc2da4b52ec235340b82ba811eab5e73ad
gosted <9172940+du-hao-111@user.noreply.gitee.com>
2022-09-21 16:17:54 +0800

    react-router-dom学习 基础demo

--- react-demo/my-app/src/index.js
+++ react-demo/my-app/src/index.js
@@ -1,160 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect, useDebugValue } from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 
-// class Square extends React.Component {
-//   render() {
-//     return (
-//       <button
-//         className="square"
-//         onClick={() => {
-//           // console.log('click')
-//           this.props.onClick()
-//         }}
-//       >
-//         {this.props.value}
-//       </button>
-//     )
-//   }
-// }
-
-function Square(props) {
+function Example2() {
   return (
-    <button className="square" style={props.highlight ? { color: 'red' } : {}} onClick={props.onClick}>
-      {props.value}
-    </button>
+    <div
+      tabIndex={1}
+      onFocus={(e) => {
+        if (e.currentTarget === e.target) {
+          console.log('focused self')
+        } else {
+          console.log('focused child', e.target)
+        }
+        if (!e.currentTarget.contains(e.relatedTarget)) {
+          // Not triggered when swapping focus between children
+          console.log('focus entered self')
+        }
+      }}
+      onBlur={(e) => {
+        console.log(e)
+        if (e.currentTarget === e.target) {
+          console.log('unfocused self')
+        } else {
+          console.log('unfocused child', e.target)
+        }
+        if (!e.currentTarget.contains(e.relatedTarget)) {
+          // Not triggered when swapping focus between children
+          console.log('focus left self')
+        }
+      }}
+    >
+      <input id="1" />
+      <input id="2" />
+    </div>
   )
 }
 
-class Board extends React.Component {
-  renderSquare(i, highlight) {
-    return <Square value={this.props.squares[i]} highlight={highlight} key={i} onClick={() => this.props.onClick(i)} />
-  }
+function Example() {
+  const [count, setCount] = useState({ count: 0 })
+  const [count2, setCount2] = useState(0)
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     setCount2(count2 + 1)
+  //   }, 1000)
+  // })
 
-  render() {
-    const squares = []
-    for (let i = 0; i < 3; i++) {
-      const row = []
-      for (let j = 0; j < 3; j++) {
-        row.push(this.renderSquare(i * 3 + j, this.props.highlightList.includes(i * 3 + j)))
-      }
-      squares.push(<div key={i}>{row}</div>)
-    }
-    return <div>{squares}</div>
+  const fun1 = (e) => {
+    console.log(e)
+    setCount({ count: count.count + 1 })
   }
+  const fun2 = () => {
+    setCount2(count2 + 1)
+    // count.count += 1
+  }
+  return (
+    <div>
+      <p>You clicked {count.count} times</p>
+      <p>You clicked {count2} times</p>
+      <button onClick={(e) => fun1(e)}>Click me</button>
+      <button onClick={fun2}>Click me</button>
+      <Example2></Example2>
+    </div>
+  )
 }
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null),
-          row: -1,
-          col: -1,
-        },
-      ],
-      reverse: false,
-      stepNumber: 0,
-      xIsNext: true,
-    }
-  }
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1)
-    const current = history[history.length - 1]
-    const squares = current.squares.slice()
-    if (calculateWinner(squares).winner || squares[i]) {
-      return
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O'
-    const row = ((i / 3) | 0) + 1
-    const col = (i % 3) + 1
-    this.setState({
-      history: history.concat([{ squares, row, col }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    })
-  }
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: step % 2 === 0,
-    })
-  }
-  reverseHistory() {
-    this.setState({
-      reverse: !this.state.reverse,
-    })
-  }
-  render() {
-    const history = this.state.history
-    const current = history[this.state.stepNumber]
-    const { winner, line } = calculateWinner(current.squares)
-
-    const moves = (this.state.reverse ? history.slice().reverse() : history).map((step, move) => {
-      let desc
-      move = this.state.reverse ? history.length - 1 - move : move
-      if (move === 0) {
-        desc = 'Go to game start'
-      } else {
-        desc = `Go to move # ${this.state.xIsNext ? 'X' : 'Y'} (row: ${step.row}, col: ${step.col})`
-      }
-
-      return (
-        <li key={move}>
-          <button style={this.state.stepNumber === move ? { fontWeight: 800 } : {}} onClick={() => this.jumpTo(move)}>
-            {desc}
-          </button>
-        </li>
-      )
-    })
-
-    let status
-    if (winner) {
-      status = 'Winner: ' + winner
-    } else if (this.state.stepNumber === 9) {
-      status = 'It ends in a draw'
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
-    }
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board squares={current.squares} highlightList={line} onClick={(i) => this.handleClick(i)} />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <button onClick={() => this.reverseHistory()}>反转历史记录</button>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    )
-  }
+function App() {
+  return <Example />
 }
 
-// @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Game />)
-
-// 计算胜者，8种胜利方式
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i]
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return { winner: squares[a], line: lines[i] }
-    }
-  }
-  return { winner: null, line: [] }
-}
+root.render(<App />)
---
+++ react-demo/router-study/src/main.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from 'react-router-dom'
+import './index.css'
+import Root, { loader as rootLoader, action as rootAction } from './routes/root'
+import ErrorPage from './error-page'
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from './routes/contact'
+import EditContact, { action as editAction } from './routes/edit'
+import { action as destroyAction } from './routes/destroy'
+import Index from './routes'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: 'contacts/:contactId',
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: 'contacts/:contactId/edit',
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: 'contacts/:contactId/destroy',
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ],
+      },
+    ],
+  },
+])
+
+const router2 = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route path="contacts/:contactId" action={destroyAction} />
+      </Route>
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router2} />
+  </React.StrictMode>
+)
